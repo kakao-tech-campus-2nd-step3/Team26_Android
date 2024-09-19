dcb08a4ffe61367d11a1fe628e828f9181b74578
Lorenzo <tnalsdlekgsk1@icloud.com>
2024-09-19 21:26:17 +0900

    feat: 홈 큐레이션 뷰 UI  추가 - xml 파일을 추가했습니다.

---
+++ app/src/main/java/org/ktc2/cokaen/wouldyouin/HomeCurationActivity.kt
@@ -0,0 +1,20 @@
+package org.ktc2.cokaen.wouldyouin
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class HomeCurationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_home_curation)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
