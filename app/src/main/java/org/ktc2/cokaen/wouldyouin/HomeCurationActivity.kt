1b67d7ed9a0c7c9342c83f5ddcb0e07e6ccce798
Lorenzo <tnalsdlekgsk1@icloud.com>
2024-09-27 03:02:58 +0900

    feat: 바인딩 코드 추가, 불필요한 코드 삭제

--- app/src/main/java/org/ktc2/cokaen/wouldyouin/HomeCurationActivity.kt
+++ app/src/main/java/org/ktc2/cokaen/wouldyouin/HomeCurationActivity.kt
@@ -5,16 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import org.ktc2.cokaen.wouldyouin.databinding.ActivityHomeCurationBinding
 
 class HomeCurationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeCurationBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_home_curation)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_home_curation)
     }
 }
--- app/src/main/java/org/ktc2/cokaen/wouldyouin/LikedOrganizersActivity.kt
+++ app/src/main/java/org/ktc2/cokaen/wouldyouin/LikedOrganizersActivity.kt
@@ -1,20 +1,20 @@
 package org.ktc2.cokaen.wouldyouin
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import org.ktc2.cokaen.wouldyouin.databinding.ActivityLikedOrganizersBinding
 
 class LikedOrganizersActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLikedOrganizersBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_liked_organizers)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_liked_organizers)
+
     }
 }
