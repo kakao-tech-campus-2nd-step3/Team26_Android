0394da312ff40101fbdcc0e8c2644d63f0c437d0
Lorenzo <tnalsdlekgsk1@icloud.com>
2024-10-04 04:47:38 +0900

    feat: xml 데이터바인딩 수정, StatisticsActivity 바인딩 코드 추가

--- app/src/main/java/org/ktc2/cokaen/wouldyouin/StatisticsActivity.kt
+++ app/src/main/java/org/ktc2/cokaen/wouldyouin/StatisticsActivity.kt
@@ -5,16 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import org.ktc2.cokaen.wouldyouin.databinding.ActivityStatisticsBinding
 
 class StatisticsActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityStatisticsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_statistics)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_statistics)
     }
 }
