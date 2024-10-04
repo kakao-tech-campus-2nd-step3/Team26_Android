ae07c6e892e967a98a5f5abb885c9198c44f8ef4
LEEMINSEO <104787304+LEEMINSEO00@users.noreply.github.com>
2024-10-04 21:41:04 +0900

    [Weekly/5/Feature/Category] 상세 카테고리 구현 (#24)
    * feat: 상세 카테고리 구현
    
    * feat: 상세 카테고리 데이터 바인딩

---
+++ app/src/main/java/org/ktc2/cokaen/wouldyouin/CategoryActivity.kt
@@ -0,0 +1,18 @@
+package org.ktc2.cokaen.wouldyouin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import org.ktc2.cokaen.wouldyouin.databinding.ActivityCategoryBinding
+import org.ktc2.cokaen.wouldyouin.databinding.ActivitySearchBinding
+
+class CategoryActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCategoryBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_category)
+        binding.category = this
+    }
+}
