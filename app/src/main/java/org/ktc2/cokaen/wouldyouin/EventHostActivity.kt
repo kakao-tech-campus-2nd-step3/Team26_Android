c0fc60e5de2f0cb545accee343a4cb14ed7eb06e
ariimo <memoarim01@gmail.com>
2024-10-08 00:34:53 +0900

    전남대Android_26조_5주차 작업사항 (#29)
    * [Weekly/5/Feature/EventUI] 이벤트 등록 화면 UI 구현 (#22)
    
    * feat: 주최자 통계 페이지 UI 구현
    - UI를 구현하였습니다.
    - 차트 그래프 구현을 위해, MPAndroidChart 라이브러리를 추가하였습니다.
    
    * feat: xml 데이터바인딩 수정, StatisticsActivity 바인딩 코드 추가
    
    * feat: 이벤트 등록 화면 UI 구현
    - xml 파일 추가
    - 바인딩 코드 추가
    
    * [Weekly/5/Feature/Category] 상세 카테고리 구현 (#24)
    
    * feat: 상세 카테고리 구현
    
    * feat: 상세 카테고리 데이터 바인딩
    
    * [Weekly/5/Feature/EventHost] 주최자 공연 상세 페이지 구현 (#25)
    
    - xml 구현
    - 데이터 바인딩 적용
    
    * [Weekly/5/Feature/PostedEvent]게시한 행사 구현 (#26)
    
    - xml 구현
    - 데이터 바인딩
    
    ---------
    
    Co-authored-by: suminjang <127755029+sumintnals@users.noreply.github.com>
    Co-authored-by: LEEMINSEO <104787304+LEEMINSEO00@users.noreply.github.com>

---
+++ app/src/main/java/org/ktc2/cokaen/wouldyouin/EventHostActivity.kt
@@ -0,0 +1,18 @@
+package org.ktc2.cokaen.wouldyouin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import org.ktc2.cokaen.wouldyouin.databinding.ActivityEventHostBinding
+import org.ktc2.cokaen.wouldyouin.databinding.ActivitySearchBinding
+
+class EventHostActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEventHostBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_event_host)
+        binding.eventhost = this
+    }
+}
---
+++ app/src/main/java/org/ktc2/cokaen/wouldyouin/PostedEventActivity.kt
@@ -0,0 +1,18 @@
+package org.ktc2.cokaen.wouldyouin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import org.ktc2.cokaen.wouldyouin.databinding.ActivityPostedEventBinding
+import org.ktc2.cokaen.wouldyouin.databinding.ActivitySearchBinding
+
+class PostedEventActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPostedEventBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_posted_event)
+        binding.postedevent = this
+    }
+}
