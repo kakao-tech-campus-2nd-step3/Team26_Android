c1404e0747d3772f13099840700cd4088c753be9
LEEMINSEO <real2ms@naver.com>
2024-10-11 00:59:44 +0900

    feat: 하단바 및 프래그먼트 일부 적용 - 오류 및 필요 없는 코드 삭 - 하단바 구현 - 일부 activity 및 xml 파일 프래그먼트화 - 검색, 프로필 페이지 프래그먼트 적용(하단바 아이콘 클릭 시 페이지 변경)

---
+++ app/src/main/java/org/ktc2/cokaen/wouldyouin/SearchFragment.kt
@@ -0,0 +1,23 @@
+package org.ktc2.cokaen.wouldyouin
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
+import org.ktc2.cokaen.wouldyouin.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+
+    private lateinit var binding: FragmentSearchBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_search, container, false)
+
+        return binding.root
+    }
+}
