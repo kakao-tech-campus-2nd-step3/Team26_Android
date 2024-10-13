91839f4d7064a1c608e7a94d3b76f276be876818
suminjang <127755029+sumintnals@users.noreply.github.com>
2024-10-13 18:01:47 +0900

    [Weekly/7/Feature/Navigation] 내비게이션 연결 및 빌드 오류 수정 (#37)
    feat: BookingFragment 추가
    
    feat: 네비게이션 연결
    
    feat: 네비게이션 연결
    
    feat: BottomNavigation과 화면 연결

--- app/src/main/java/org/ktc2/cokaen/wouldyouin/MainActivity.kt
+++ app/src/main/java/org/ktc2/cokaen/wouldyouin/MainActivity.kt
@@ -1,42 +1,60 @@
 package org.ktc2.cokaen.wouldyouin
 
+import NavigationHandler
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import org.ktc2.cokaen.wouldyouin.core_navigation.NavigationCommandBuilder
+import org.ktc2.cokaen.wouldyouin.core_navigation.NavigationDestination
 import org.ktc2.cokaen.wouldyouin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navigationHandler: NavigationHandler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar?.hide()
 
-        // 기본 화면은 SearchFragment로 설정
-        openFragment(SearchFragment())
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        navigationHandler = NavigationHandler(this, navController)
 
-        binding.bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+        binding.bottomNavigationView.setupWithNavController(navController)
+
+        binding.bottomNavigationView.selectedItemId = R.id.nav_home
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.nav_search -> {
-                    openFragment(SearchFragment())
-                    true
-                }
-                R.id.nav_profile -> {
-                    openFragment(AccountFragment())
-                    true
-                }
+                R.id.nav_search -> navigateToFragment(org.ktc2.cokaen.wouldyouin.core.navigation.R.string.search_deeplink_url)
+                R.id.nav_book -> navigateToFragment(org.ktc2.cokaen.wouldyouin.core.navigation.R.string.booking_deeplink_url)
+                R.id.nav_home -> navigateToFragment(org.ktc2.cokaen.wouldyouin.core.navigation.R.string.home_curation_deeplink_url)
+                R.id.nav_like -> navigateToFragment(org.ktc2.cokaen.wouldyouin.core.navigation.R.string.likes_deeplink_url)
+                R.id.nav_profile -> navigateToFragment(org.ktc2.cokaen.wouldyouin.core.navigation.R.string.profile_deeplink_url)
                 else -> false
             }
+            true
         }
     }
 
-    private fun openFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.fragment_container, fragment)
-            .addToBackStack(null)
-            .commit()
+    private fun navigateToFragment(deepLinkResId: Int): Boolean {
+        val command = NavigationCommandBuilder()
+            .to(NavigationDestination.Fragment(deepLinkResId))
+            .build()
+        navigationHandler.navigate(command)
+        return true
     }
 }
+
+//    private fun openFragment(fragment: Fragment) {
+//        supportFragmentManager.beginTransaction()
+//            .replace(R.id.fragment_container, fragment)
+//            .addToBackStack(null)
+//            .commit()
+//    }
--- app/src/main/java/org/ktc2/cokaen/wouldyouin/SearchActivity.kt
+++ app/src/main/java/org/ktc2/cokaen/wouldyouin/SearchActivity.kt
@@ -1,19 +1,19 @@
-
-package org.ktc2.cokaen.wouldyouin
-
-import android.os.Bundle
-import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import org.ktc2.cokaen.wouldyouin.databinding.ActivitySearchBinding
-
-class SearchActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivitySearchBinding
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_search)
-        binding.search = this
-    }
-}
+//
+//package org.ktc2.cokaen.wouldyouin
+//
+//import android.os.Bundle
+//import android.util.Log
+//import androidx.appcompat.app.AppCompatActivity
+//import androidx.databinding.DataBindingUtil
+//import org.ktc2.cokaen.wouldyouin.databinding.ActivitySearchBinding
+//
+//class SearchActivity : AppCompatActivity() {
+//
+//    private lateinit var binding: ActivitySearchBinding
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        binding = DataBindingUtil.setContentView(this, R.layout.activity_search)
+//        binding.search = this
+//    }
+//}
