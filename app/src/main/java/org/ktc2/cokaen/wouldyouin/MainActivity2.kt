6bdc52a14af7dbee9eb2c6defb365ca70c9feae5
suminjang <127755029+sumintnals@users.noreply.github.com>
2024-10-13 18:01:47 +0900

    [Weekly/7/Feature/Navigation] 내비게이션 연결 및 빌드 오류 수정 (#37)
    feat: BookingFragment 추가
    
    feat: 네비게이션 연결
    
    feat: 네비게이션 연결
    
    feat: BottomNavigation과 화면 연결

--- app/src/main/java/org/ktc2/cokaen/wouldyouin/MainActivity2.kt
+++ app/src/main/java/org/ktc2/cokaen/wouldyouin/MainActivity2.kt
@@ -1,43 +1,43 @@
-package org.ktc2.cokaen.wouldyouin
-
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupWithNavController
-import org.ktc2.cokaen.wouldyouin.databinding.ActivityMain2Binding
-
-class MainActivity2 : AppCompatActivity() {
-
-    private lateinit var navController: NavController
-    private lateinit var binding: ActivityMain2Binding  // 데이터 바인딩 객체
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // 데이터 바인딩 객체 초기화
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main2)
-
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        navController = navHostFragment.navController
-
-        binding.bottomNav.setupWithNavController(navController)
-
-        // 바텀 내비게이션 아이템 선택 시 액션 정의
-        binding.bottomNav.setOnNavigationItemSelectedListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.navigation_home -> {
-                    navController.navigate(R.id.navigation_home)
-                    true
-                }
-                R.id.navigation_likes -> {
-                    navController.navigate(R.id.navigation_likes)
-                    true
-                }
-                else -> false
-            }
-        }
-    }
-}
+//package org.ktc2.cokaen.wouldyouin
+//
+//import android.os.Bundle
+//import androidx.appcompat.app.AppCompatActivity
+//import androidx.databinding.DataBindingUtil
+//import androidx.navigation.NavController
+//import androidx.navigation.fragment.NavHostFragment
+//import androidx.navigation.ui.setupWithNavController
+//import org.ktc2.cokaen.wouldyouin.databinding.ActivityMain2Binding
+//
+//class MainActivity2 : AppCompatActivity() {
+//
+//    private lateinit var navController: NavController
+//    private lateinit var binding: ActivityMain2Binding  // 데이터 바인딩 객체
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//
+//        // 데이터 바인딩 객체 초기화
+//        binding = DataBindingUtil.setContentView(this, R.layout.activity_main2)
+//
+//        val navHostFragment = supportFragmentManager
+//            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+//        navController = navHostFragment.navController
+//
+//        binding.bottomNav.setupWithNavController(navController)
+//
+//        // 바텀 내비게이션 아이템 선택 시 액션 정의
+//        binding.bottomNav.setOnNavigationItemSelectedListener { menuItem ->
+//            when (menuItem.itemId) {
+//                R.id.navigation_home -> {
+//                    navController.navigate(R.id.navigation_home)
+//                    true
+//                }
+//                R.id.navigation_likes -> {
+//                    navController.navigate(R.id.navigation_likes)
+//                    true
+//                }
+//                else -> false
+//            }
+//        }
+//    }
+//}
