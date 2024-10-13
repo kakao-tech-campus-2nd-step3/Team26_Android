dc46fda2d65bea5ae8b055074a768355c79d9d59
suminjang <127755029+sumintnals@users.noreply.github.com>
2024-10-13 18:01:47 +0900

    feat: BookingFragment 추가

--- app/src/main/java/org/ktc2/cokaen/wouldyouin/MainActivity.kt
+++ app/src/main/java/org/ktc2/cokaen/wouldyouin/MainActivity.kt
@@ -1,9 +1,14 @@
 package org.ktc2.cokaen.wouldyouin
 
+import NavigationHandler
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import org.ktc2.cokaen.wouldyouin.core_navigation.NavigationCommandBuilder
+import org.ktc2.cokaen.wouldyouin.core_navigation.NavigationDestination
 import org.ktc2.cokaen.wouldyouin.databinding.ActivityMainBinding
+import org.ktc2.cokaen.wouldyouin.feat_curation.view.HomeCurationFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,17 +20,35 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // 기본 화면은 SearchFragment로 설정
-        openFragment(SearchFragment())
+        lateinit var navigationHandler: NavigationHandler
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        navigationHandler = NavigationHandler(this, navController)
+
+//        // 기본 화면은 SearchFragment로 설정
+//        openFragment(SearchFragment())
 
         binding.bottomNavigationView.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.nav_search -> {
-                    openFragment(SearchFragment())
+                    navigateToFragment(org.ktc2.cokaen.wouldyouin.core.navigation.R.string.search_deeplink_url, navigationHandler)
+                    true
+                }
+                R.id.nav_book -> {
+                    navigateToFragment(org.ktc2.cokaen.wouldyouin.core.navigation.R.string.booking_deeplink_url, navigationHandler)
+                    true
+                }
+                R.id.nav_home -> {
+                    navigateToFragment(org.ktc2.cokaen.wouldyouin.core.navigation.R.string.home_curation_deeplink_url, navigationHandler)
+                    true
+                }
+                R.id.nav_like -> {
+                    navigateToFragment(org.ktc2.cokaen.wouldyouin.core.navigation.R.string.booking_deeplink_url, navigationHandler)
                     true
                 }
                 R.id.nav_profile -> {
-                    openFragment(AccountFragment())
+                    navigateToFragment(org.ktc2.cokaen.wouldyouin.core.navigation.R.string.profile_deeplink_url, navigationHandler)
                     true
                 }
                 else -> false
@@ -38,5 +61,12 @@ class MainActivity : AppCompatActivity() {
             .replace(R.id.fragment_container, fragment)
             .addToBackStack(null)
             .commit()
+    }
+
+    private fun navigateToFragment(deepLinkResId: Int, navigationHandler: NavigationHandler) {
+        val command = NavigationCommandBuilder()
+            .to(NavigationDestination.Fragment(deepLinkResId))
+            .build()
+        navigationHandler.navigate(command)
     }
 }
---
+++ feat-booking/src/main/java/org/ktc2/cokaen/wouldyouin/feat_booking/view/BookingFragment.kt
@@ -0,0 +1,44 @@
+package org.ktc2.cokaen.wouldyouin.feat_booking.view
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import org.ktc2.cokaen.wouldyouin.feat_booking.R
+import org.ktc2.cokaen.wouldyouin.feat_booking.databinding.FragmentBookingBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [BookingFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class BookingFragment : Fragment() {
+    private var _binding: FragmentBookingBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentBookingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d("test", "booking")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
