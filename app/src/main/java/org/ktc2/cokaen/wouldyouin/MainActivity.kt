37b92b4cd4a51b6fb6ec5f24882768e4292955bc
suminjang <127755029+sumintnals@users.noreply.github.com>
2024-10-13 19:29:12 +0900

    feat: BottomNavigation과 화면 연결

--- app/src/main/java/org/ktc2/cokaen/wouldyouin/MainActivity.kt
+++ app/src/main/java/org/ktc2/cokaen/wouldyouin/MainActivity.kt
@@ -5,14 +5,15 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import org.ktc2.cokaen.wouldyouin.core_navigation.NavigationCommandBuilder
 import org.ktc2.cokaen.wouldyouin.core_navigation.NavigationDestination
 import org.ktc2.cokaen.wouldyouin.databinding.ActivityMainBinding
-import org.ktc2.cokaen.wouldyouin.feat_curation.view.HomeCurationFragment
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navigationHandler: NavigationHandler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,41 +22,35 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         supportActionBar?.hide()
 
-        lateinit var navigationHandler: NavigationHandler
-
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         navigationHandler = NavigationHandler(this, navController)
 
-//        // 기본 화면은 SearchFragment로 설정
-//        openFragment(SearchFragment())
+        binding.bottomNavigationView.setupWithNavController(navController)
 
-        binding.bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+        binding.bottomNavigationView.selectedItemId = R.id.nav_home
+
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.nav_search -> {
-                    navigateToFragment(org.ktc2.cokaen.wouldyouin.core.navigation.R.string.search_deeplink_url, navigationHandler)
-                    true
-                }
-                R.id.nav_book -> {
-                    navigateToFragment(org.ktc2.cokaen.wouldyouin.core.navigation.R.string.booking_deeplink_url, navigationHandler)
-                    true
-                }
-                R.id.nav_home -> {
-                    navigateToFragment(org.ktc2.cokaen.wouldyouin.core.navigation.R.string.home_curation_deeplink_url, navigationHandler)
-                    true
-                }
-                R.id.nav_like -> {
-                    navigateToFragment(org.ktc2.cokaen.wouldyouin.core.navigation.R.string.likes_deeplink_url, navigationHandler)
-                    true
-                }
-                R.id.nav_profile -> {
-                    navigateToFragment(org.ktc2.cokaen.wouldyouin.core.navigation.R.string.profile_deeplink_url, navigationHandler)
-                    true
-                }
+                R.id.nav_search -> navigateToFragment(org.ktc2.cokaen.wouldyouin.core.navigation.R.string.search_deeplink_url)
+                R.id.nav_book -> navigateToFragment(org.ktc2.cokaen.wouldyouin.core.navigation.R.string.booking_deeplink_url)
+                R.id.nav_home -> navigateToFragment(org.ktc2.cokaen.wouldyouin.core.navigation.R.string.home_curation_deeplink_url)
+                R.id.nav_like -> navigateToFragment(org.ktc2.cokaen.wouldyouin.core.navigation.R.string.likes_deeplink_url)
+                R.id.nav_profile -> navigateToFragment(org.ktc2.cokaen.wouldyouin.core.navigation.R.string.profile_deeplink_url)
                 else -> false
             }
+            true
         }
     }
+
+    private fun navigateToFragment(deepLinkResId: Int): Boolean {
+        val command = NavigationCommandBuilder()
+            .to(NavigationDestination.Fragment(deepLinkResId))
+            .build()
+        navigationHandler.navigate(command)
+        return true
+    }
+}
 
 //    private fun openFragment(fragment: Fragment) {
 //        supportFragmentManager.beginTransaction()
@@ -63,11 +58,3 @@ class MainActivity : AppCompatActivity() {
 //            .addToBackStack(null)
 //            .commit()
 //    }
-
-    private fun navigateToFragment(deepLinkResId: Int, navigationHandler: NavigationHandler) {
-        val command = NavigationCommandBuilder()
-            .to(NavigationDestination.Fragment(deepLinkResId))
-            .build()
-        navigationHandler.navigate(command)
-    }
-}
