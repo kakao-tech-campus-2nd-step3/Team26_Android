0b6804daf85947ee17b1032521730f7f13a7d5df
suminjang <127755029+sumintnals@users.noreply.github.com>
2024-10-25 23:42:29 +0900

    [Weekly/8/Feature/Retrofit] 데이터클래스 추가 및 레트로핏 설정 (#44)

---
+++ network/src/main/java/org/ktc2/cokaen/wouldyouin/network/KakaoAPIRetrofitClient.kt
@@ -0,0 +1,23 @@
+package org.ktc2.cokaen.wouldyouin.network
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import org.ktc2.cokaen.wouldyouin.network.BuildConfig
+import retrofit2.converter.gson.GsonConverterFactory
+
+object KakaoAPIRetrofitClient {
+    private const val BASE_URL = "https://dapi.kakao.com/"
+
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(AuthInterceptor(BuildConfig.KAKAO_REST_API_KEY))
+        .build()
+
+    val retrofitService: KakaoAPIRetrofitService by lazy(LazyThreadSafetyMode.SYNCHRONIZED) {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(KakaoAPIRetrofitService::class.java)
+    }
+}
---
+++ network/src/main/java/org/ktc2/cokaen/wouldyouin/network/ServerAPIRetrofitService.kt
@@ -0,0 +1,8 @@
+package org.ktc2.cokaen.wouldyouin.network
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+interface ServerAPIRetrofitService {
+
+}
