7ffaac732fb6dc4e51a61016a213576bd9ba05ef
LEEMINSEO <104787304+LEEMINSEO00@users.noreply.github.com>
2024-10-25 23:32:22 +0900

    [Weekly/8/Feature/ScreenConnection] 프래그먼트화 및 화면 연결 (#42)
    * feat: 화면 연결
    -검색 -> 카테고리 -> 행사 목록
    -행사 목록 -> 지도뷰
    
    * feat: 프래그먼트 수정 및 화면 연결
    -Category 프래그먼트화
    -Map 프래그먼트화
    -이슈: 버튼 클릭 후 맵뷰로 넘어갈 때 테마 문제로 인해 앱이 종료됨

---
+++ feat-event/src/main/java/org/ktc2/cokaen/wouldyouin/feat_event/view/MapFragment.kt
@@ -0,0 +1,26 @@
+package org.ktc2.cokaen.wouldyouin.feat_event.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
+import org.ktc2.cokaen.wouldyouin.feat_event.R
+import org.ktc2.cokaen.wouldyouin.feat_event.databinding.FragmentMapBinding
+
+class MapFragment : Fragment() {
+
+    private lateinit var binding: FragmentMapBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_map, container, false)
+        binding.map = this
+
+        return binding.root
+    }
+}
