8aba07fda72796cbc9a40c1660a57a8cc48ce8ff
suminjang <127755029+sumintnals@users.noreply.github.com>
2024-10-31 01:17:02 +0900

    feat: 큐레이션 작성 화면 연결

--- feat-curation/src/main/java/org/ktc2/cokaen/wouldyouin/feat_curation/view/HomeCurationFragment.kt
+++ feat-curation/src/main/java/org/ktc2/cokaen/wouldyouin/feat_curation/view/HomeCurationFragment.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import androidx.databinding.DataBindingUtil
 import org.ktc2.cokaen.wouldyouin.feat_curation.R
 import org.ktc2.cokaen.wouldyouin.feat_curation.databinding.FragmentHomeCurationBinding
 
--- feat-likes/src/main/java/com/example/feat_likes/adapter/CurationAdapter.kt
+++
@@ -1,7 +0,0 @@
-package com.example.feat_likes.adapter
-
-import android.view.LayoutInflater
-import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
-import org.ktc2.cokaen.wouldyouin.data.model.Curation
-
