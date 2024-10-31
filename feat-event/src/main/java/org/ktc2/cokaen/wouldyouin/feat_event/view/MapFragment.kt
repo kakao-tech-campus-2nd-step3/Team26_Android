7c7b49307534b210ba97bc9695b9715e16b57005
LEEMINSEO <real2ms@naver.com>
2024-10-31 23:28:48 +0900

    feat: 행사 상세 지도 뷰에 카카오맵 적용
    -지도로 보기 버튼 클릭 시 지도 배경에 카카오맵 적용

--- feat-event/src/main/java/org/ktc2/cokaen/wouldyouin/feat_event/view/MapFragment.kt
+++ feat-event/src/main/java/org/ktc2/cokaen/wouldyouin/feat_event/view/MapFragment.kt
@@ -6,13 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
+import com.kakao.vectormap.KakaoMap
+import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.MapLifeCycleCallback
+import com.kakao.vectormap.MapView
 import org.ktc2.cokaen.wouldyouin.feat_event.R
 import org.ktc2.cokaen.wouldyouin.feat_event.databinding.FragmentMapBinding
 
 class MapFragment : Fragment() {
 
     private lateinit var binding: FragmentMapBinding
-
+    private lateinit var mapView: MapView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +25,30 @@ class MapFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_map, container, false)
         binding.map = this
 
+        mapView = binding.mapView
+        mapView.start(object : MapLifeCycleCallback() {
+            override fun onMapDestroy() {
+                // 지도 API가 정상적으로 종료될 때 호출됨
+            }
+
+            override fun onMapError(error: Exception?) {
+                // 인증 실패 및 지도 사용 중 에러가 발생할 때 호출됨
+            }
+        }, object : KakaoMapReadyCallback() {
+            override fun onMapReady(kakaoMap: KakaoMap) {
+                // 인증 후 API가 정상적으로 실행될 때 호출됨
+            }
+        })
+
         return binding.root
+    }
+    override fun onResume() {
+        super.onResume()
+        mapView.resume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.pause()
     }
 }
