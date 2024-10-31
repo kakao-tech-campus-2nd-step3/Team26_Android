45d988686c3ad9681d6b126595d0585bb86c924b
LEEMINSEO <104787304+LEEMINSEO00@users.noreply.github.com>
2024-10-31 23:48:19 +0900

    [Weekly/9/Feature/ApplyKakaoMap] 행사 상세 지도 뷰에 카카오맵 적용 (#53)
    -지도로 보기 버튼 클릭 시 지도 배경에 카카오맵 적용

--- feat-event/build.gradle.kts
+++ feat-event/build.gradle.kts
@@ -1,3 +1,5 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
 plugins {
     id("wouldyouin.android.library")
     id("wouldyouin.android.dependency")
@@ -10,6 +12,9 @@ android {
 
     defaultConfig {
         consumerProguardFiles("consumer-rules.pro")
+
+        resValue("string", "kakao_api_key", getApiKey("KAKAO_API_KEY"))
+        buildConfigField("String", "KAKAO_REST_API_KEY", getApiKey("KAKAO_REST_API_KEY"))
     }
 
     buildTypes {
@@ -65,4 +70,7 @@ dependencies {
 
     // 추가
     implementation(libs.material)
+    implementation("com.kakao.sdk:v2-all:2.20.3")
+    implementation("com.kakao.maps.open:android:2.9.5")
 }
+fun getApiKey(key: String): String = gradleLocalProperties(rootDir, providers).getProperty(key)
