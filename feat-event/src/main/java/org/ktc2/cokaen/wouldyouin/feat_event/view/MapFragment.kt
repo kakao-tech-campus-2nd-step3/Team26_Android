3a9e88b7705374265392d1c27577a2f10ecbe888
LEEMINSEO <104787304+LEEMINSEO00@users.noreply.github.com>
2024-11-01 22:11:32 +0900

    [Weekly/9/Feature/MapLabel] 카카오맵 라벨 추가 (#58)
    -LodLabel 적용하여 라벨(마커) 여러 개 생성 가능

--- feat-event/src/main/java/org/ktc2/cokaen/wouldyouin/feat_event/view/MapFragment.kt
+++ feat-event/src/main/java/org/ktc2/cokaen/wouldyouin/feat_event/view/MapFragment.kt
@@ -1,5 +1,7 @@
 package org.ktc2.cokaen.wouldyouin.feat_event.view
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,8 +10,15 @@ import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
 import com.kakao.vectormap.KakaoMap
 import com.kakao.vectormap.KakaoMapReadyCallback
+import com.kakao.vectormap.LatLng
 import com.kakao.vectormap.MapLifeCycleCallback
 import com.kakao.vectormap.MapView
+import com.kakao.vectormap.RoadViewRequest
+import com.kakao.vectormap.camera.CameraUpdateFactory
+import com.kakao.vectormap.label.LabelOptions
+import com.kakao.vectormap.label.LabelStyle
+import com.kakao.vectormap.label.LabelStyles
+import org.ktc2.cokaen.wouldyouin.data.model.Location
 import org.ktc2.cokaen.wouldyouin.feat_event.R
 import org.ktc2.cokaen.wouldyouin.feat_event.databinding.FragmentMapBinding
 
@@ -17,7 +26,12 @@ class MapFragment : Fragment() {
 
     private lateinit var binding: FragmentMapBinding
     private lateinit var mapView: MapView
-
+    private val locations = listOf(
+        Location(name = "행사 장소 1", latitude = 37.5665, longitude = 126.9780),
+        Location(name = "행사 장소 2", latitude = 37.5655, longitude = 126.9770),
+        Location(name = "행사 장소 3", latitude = 37.5645, longitude = 126.9760)
+    )
+    
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -37,11 +51,34 @@ class MapFragment : Fragment() {
         }, object : KakaoMapReadyCallback() {
             override fun onMapReady(kakaoMap: KakaoMap) {
                 // 인증 후 API가 정상적으로 실행될 때 호출됨
+
+                val lodLabelLayer = kakaoMap.labelManager?.lodLayer
+
+                locations.forEach { location ->
+                    val markerBitmap = BitmapFactory.decodeResource(resources, R.drawable.marker)
+                    val scaledBitmap = Bitmap.createScaledBitmap(markerBitmap, 50, 50, true) // 50x50으로 조정
+
+                    val labelStyle = LabelStyle.from(scaledBitmap)
+                    val labelStyles = LabelStyles.from(labelStyle)
+
+                    val options = LabelOptions.from(LatLng.from(location.latitude, location.longitude))
+                        .setStyles(labelStyles)
+
+                    lodLabelLayer?.addLodLabel(options)
+                }
+
+                if (locations.isNotEmpty()) {
+                    val firstLocation = locations[0]
+                    kakaoMap.moveCamera(CameraUpdateFactory.newCenterPosition(LatLng.from(firstLocation.latitude, firstLocation.longitude), 15))
+                }
+
+
             }
         })
 
         return binding.root
     }
+
     override fun onResume() {
         super.onResume()
         mapView.resume()
