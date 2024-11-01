bb874e07426b8e976efc222c37dafb08e4e080dc
LEEMINSEO <104787304+LEEMINSEO00@users.noreply.github.com>
2024-11-01 22:05:04 +0900

    [Weekly/9/Feature/EventRecycler] 행사 리사이클러뷰 어댑터 연결 (#56)
    * feat: 행사 요약 정보 요소 추가
    
    -예약 인원 추가
    
    * [Weekly/9/Feature/ReservationPage] 예매 페이지 구현 (#50)
    
    * feat: 예매 페이지 구현
    
    -activity_booking.xml
    -Number Picker를 사용하여 매수 적용
    -매수에 따른 값 계산
    -결제하기 버튼 클릭 시 결제 URL로 이동
    
    * feat: 행사 상세 페이지&예매 페이지 연결
    
    -행사 상세 페이지의 예매하기 버튼 클릭 시 예매 페이지로 연결
    
    * [Feature/DataClass] 행사, 예매 데이터 클래스 생성 (#51)
    
    * feat: 행사 데이터 클래스 생성
    
    -Event.kt
    -Location.kt
    
    * feat: 예약 데이터 클래스 생성
    
    -Reservation.kt
    -DTO 상에서는 언더스코어를 사용했지만 코틀린에서는 카멜케이스를 권장하기 때문에 변수명을 수정함
    -언더스코어를 사용할 경우 어노테이션을 사용하면 됨(추후 상의하기)
    
    * [Weekly/9/Feature/ScreenConnection] 화면 연결 (#52)
    
    * feat: 화면 연결
    -검색 -> 카테고리 -> 행사 목록
    -행사 목록 -> 지도뷰
    
    * feat: 프래그먼트 수정 및 화면 연결
    -Category 프래그먼트화
    -Map 프래그먼트화
    -이슈: 버튼 클릭 후 맵뷰로 넘어갈 때 테마 문제로 인해 앱이 종료됨
    
    * [Weekly/9/Feature/ApplyKakaoMap] 행사 상세 지도 뷰에 카카오맵 적용 (#53)
    
    -지도로 보기 버튼 클릭 시 지도 배경에 카카오맵 적용
    
    * feat: 행사 상세 페이지의 위치 정보에 카카오맵 적용 (#54)
    
    * [Feat]온보딩 구현, 이미지url xml 연결 기능 구현 (#55)
    
    * feat: 테마 변경, 드롭다운 메뉴 구현
    
    * feat: 이미지 바인딩 어댑터 추가
    
    - ImageView에 Glide 라이브러리를 사용하여 이미지 url을 전달하는 바인딩 어댑터를 추가
    
    * feat: CurationAdapter추가, 데이터 바인딩
    
    * feat: 네비게이션 수정
    
    - hilt 의존성 주입해서 사용하도록 수정했습니다.
    
    * feat: 큐레이션 작성 화면 연결
    
    * feat: Location 데이터 클래스 추가
    
    * feat: 온보딩 모듈 추가, 기본 화면 작업
    
    * feat: 위치 권한 받는 프래그먼트 작성
    
    * feat: 온보딩 기능 구현 및 화면 연결, 첫 실행시에만 뜨도록 구현
    
    * feat: 큐레이션 데이터 구조 수정, 이미지 업로드 위한 함수 정의
    
    * feat: 빌드 에러 수정, MyApplication 파일 이동
    
    * feat: 행사 리사이클러뷰 어댑터 연결
    
    -임의 데이터를 넣어 리사이클러뷰 생성
    -행사 목록에서 행사 클릭 시 행사 상세 페이지로 이동
    -행사 목록의 정보를 행사 상세 페이지에 적용
    
    ---------
    
    Co-authored-by: suminjang <127755029+sumintnals@users.noreply.github.com>

--- feat-event/src/main/java/org/ktc2/cokaen/wouldyouin/feat_event/view/CategoryFragment.kt
+++ feat-event/src/main/java/org/ktc2/cokaen/wouldyouin/feat_event/view/CategoryFragment.kt
@@ -7,12 +7,17 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import org.ktc2.cokaen.wouldyouin.data.model.Event
+import org.ktc2.cokaen.wouldyouin.data.model.Location
 import org.ktc2.cokaen.wouldyouin.feat_event.R
 import org.ktc2.cokaen.wouldyouin.feat_event.databinding.FragmentCategoryBinding
+import org.ktc2.cokaen.wouldyouin.feat_event.view.adapter.EventAdapter
 
 class CategoryFragment : Fragment() {
 
     private lateinit var binding: FragmentCategoryBinding
+    private lateinit var eventAdapter: EventAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +28,38 @@ class CategoryFragment : Fragment() {
 
         binding.mapButton.setOnClickListener {
             findNavController().navigate(R.id.action_categoryFragment_to_mapFragment)
+        }
+
+        val events = listOf(
+            Event(
+                startTime = "2024-02-15 19:00",
+                endTime = "2024-02-15T21:00",
+                location = Location("광주 문화전당로", 35.1234, 126.1234),
+                title = "INTERFERENCE WAVE",
+                content = "새벽올림 겨울 공연",
+                category = "음악",
+                price = 2000,
+                eventImages = listOf("https://example.com/image.jpg"),
+                totalSeats = 35
+            ),
+            Event(
+                startTime = "2024-02-16T20:00",
+                endTime = "2024-02-16T22:00",
+                location = Location("서울 코엑스", 37.5123, 127.0565),
+                title = "Winter Fest",
+                content = "겨울 축제",
+                category = "축제",
+                price = 3000,
+                eventImages = listOf("https://example.com/image2.jpg"),
+                totalSeats = 50
+            )
+        )
+
+        eventAdapter = EventAdapter(events)
+        
+        binding.recyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = eventAdapter
         }
 
         return binding.root
