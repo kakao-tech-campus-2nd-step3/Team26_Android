32fe41a5415e8ff63ed2d03a898cc4140a44fb6e
suminjang <127755029+sumintnals@users.noreply.github.com>
2024-11-08 01:16:50 +0900

    [Feat] 액티비티 연결 (#68)
    * fix: 블럭 삭제, 작성 중 나가기 누를 시 알림창 구현
    
    * design: 텍스트필드 스타일 추가
    
    * design: 큐레이션 작성 화면 디자인 수정
    
    * [Feat] Member, Reservation, Event DTO 구현 (#66)
    
    * feat: Member, Reservation, Event DTO 구현
    
    * refactor: DTO 클래스 이름 변경 Event -> EventRequest
    
    * [Feat] 네트워크 API Service 및 Repository 생성 (#67)
    
    * refactor: 네트워크 내 디렉토리 생성하여 정리
    
    -Client, Repository, Service 디렉토리 생성
    
    * feat: 행사 API Service 생성
    
    * feat: 예약 API Service 생성
    
    * feat: 행사 API Repository 생성
    
    * feat: 예약 API Repository 생성
    
    * refactor: 지역 카테고리 내용 수정
    
    * feat: 큐레이션 작성 화면텍스트 입력 검사 위한 함수 선언
    
    * feat: EventDetailActivity -> BookingActivity 연결
    
    * feat: EventDetailActivity -> ProfileActivity 연결
    
    ---------
    
    Co-authored-by: LEEMINSEO <104787304+LEEMINSEO00@users.noreply.github.com>

--- feat-likes/src/main/java/com/example/feat_likes/viewModel/LikesViewModel.kt
+++ feat-likes/src/main/java/com/example/feat_likes/viewModel/LikesViewModel.kt
@@ -3,5 +3,5 @@ package com.example.feat_likes.viewModel
 import androidx.lifecycle.ViewModel
 
 class LikesViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    
 }
--- feat-profile/src/main/java/org/ktc2/cokaen/wouldyouin/feat_profile/view/ProfileActivity.kt
+++ feat-profile/src/main/java/org/ktc2/cokaen/wouldyouin/feat_profile/view/ProfileActivity.kt
@@ -1,19 +1,17 @@
-package org.ktc2.cokaen.wouldyouin
+package org.ktc2.cokaen.wouldyouin.feat_profile.view
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.databinding.DataBindingUtil
-import org.ktc2.cokaen.wouldyouin.databinding.ActivityProfileBinding
+import org.ktc2.cokaen.wouldyouin.feat_profile.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityProfileBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.lifecycleOwner = this
 
         //이벤트 상세 페이지에(EventDetail)서 프로필 보기를 누른 후 userId 전달 받을 경우
         //val userId = intent.getStringExtra("userId")
