db28a8319739be6554852223e6e3389ffa0e3658
LEEMINSEO <real2ms@naver.com>
2024-11-09 17:18:26 +0900

    feat: Location 데이터 클래스 수정
    -Location 데이터 클래스 내 name 변수 삭제

--- data/src/main/java/org/ktc2/cokaen/wouldyouin/data/model/Location.kt
+++ data/src/main/java/org/ktc2/cokaen/wouldyouin/data/model/Location.kt
@@ -3,7 +3,6 @@ package org.ktc2.cokaen.wouldyouin.data.model
 import com.google.gson.annotations.SerializedName
 
 data class Location(
-    val name: String,
     val latitude: Double,
     val longitude: Double
 )
