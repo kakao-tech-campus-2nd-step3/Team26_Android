50dbbd6ec878b49ef5b3701257151d11837adeae
suminjang <127755029+sumintnals@users.noreply.github.com>
2024-11-14 06:08:35 +0900

    feat: 큐레이션 작성 시 검사 기능 구현

--- feat-curation/src/main/java/org/ktc2/cokaen/wouldyouin/feat_curation/view/CreateCurationActivity.kt
+++ feat-curation/src/main/java/org/ktc2/cokaen/wouldyouin/feat_curation/view/CreateCurationActivity.kt
@@ -29,6 +29,11 @@ import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.textfield.TextInputEditText
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import com.google.android.material.textfield.TextInputEditText
 import dagger.hilt.android.AndroidEntryPoint
 import org.ktc2.cokaen.wouldyouin.core.ToastUtils
 import org.ktc2.cokaen.wouldyouin.data.entities.CurationEntity
@@ -166,7 +171,7 @@ class CreateCurationActivity : AppCompatActivity() {
     private fun setupListeners() {
         setupTextWatchers()
         setupButtons()
-        checkHashtags()
+
         binding.addCurationBlockButton.setOnClickListener {
             if (viewModel.isAddBlockButtonEnabled.value == true) {
                 Log.d("ButtonClick", "Add block button clicked")
--- feat-curation/src/main/java/org/ktc2/cokaen/wouldyouin/feat_curation/viewModel/CreateCurationViewModel.kt
+++ feat-curation/src/main/java/org/ktc2/cokaen/wouldyouin/feat_curation/viewModel/CreateCurationViewModel.kt
@@ -9,6 +9,8 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
 import android.widget.Toast
+import android.widget.EditText
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
