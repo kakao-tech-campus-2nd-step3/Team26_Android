c68fcdb976ce3176c3603a4e122f8ef1ef6a3af2
LEEMINSEO <104787304+LEEMINSEO00@users.noreply.github.com>
2024-11-15 11:33:13 +0900

    [Weekly/11Feature/Login] 로그인 화면 구현 (#84)
    * feat:빌드 오류로 인한 CuratorProfileActivity 수정
    
    * feat: 로그인 관련 구현
    -리뷰 xml 삭제
    -로그인 화면 구현(웹뷰)
    -로그인 API Service, Repository 생성

--- data/src/main/java/org/ktc2/cokaen/wouldyouin/data/model/SocialTokenResponse.kt
+++ data/src/main/java/org/ktc2/cokaen/wouldyouin/data/model/SocialTokenResponse.kt
@@ -1,5 +1,21 @@
 package org.ktc2.cokaen.wouldyouin.data.model
 
+import com.google.gson.annotations.SerializedName
+
+data class ApiResponseBodySocialTokenResponse(
+    @SerializedName("success")
+    val success: Boolean,
+
+    @SerializedName("data")
+    val data: SocialTokenResponse,
+
+    @SerializedName("code")
+    val code: String,
+
+    @SerializedName("message")
+    val message: String
+)
+
 data class SocialTokenResponse(
     val isWelcomeMember: Boolean,
     val token: String
